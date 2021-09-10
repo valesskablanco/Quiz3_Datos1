34a9dc1e5ccae28842295c236d54f354ddf74cc5
valesskablanco <valesskablanco2@gmail.com>
2021-09-09 18:00:53 -0600

    Clase para crear nodos dobles

---
+++ Queue/src/DoubleNode.java
@@ -0,0 +1,34 @@
+public class DoubleNode {
+
+    private int valor;
+    private DoubleNode next;
+    private DoubleNode previous;
+
+    public DoubleNode (int valor){
+        this.valor = valor;
+
+    }
+
+    public int getValor(){
+        return this.valor;
+
+    }
+
+    public void setNext ( DoubleNode next){
+        this.next = next;
+
+    }
+
+    public void setPrevious (DoubleNode previous){
+        this.previous = previous;
+
+    }
+
+    public DoubleNode getNext(){
+        return this.next;
+    }
+
+    public DoubleNode getPrevious(){
+        return this.previous;
+    }
+}
